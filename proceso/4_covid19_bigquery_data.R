e85514b3c0c9b46cf4e96624dcb5f61cc9269f6f
Juan E Vaccaro <102767921+jvaccaro2@users.noreply.github.com>
2023-04-09 03:47:06 -0400

    Update 4_covid19_bigquery_data.R

--- proceso/4_covid19_bigquery_data.R
+++ proceso/4_covid19_bigquery_data.R
@@ -1,5 +1,10 @@
 library(bigrquery)
 
+#Validacion de variables de control
+if(!exists("year")) year <- 2021
+if(!exists("bigquery_covid")) bigquery_covid <-  "bigquery-public-data.covid19_usafacts.summary"
+
+
 #ID del proyecto para el billing
 billing_id <- "covidtrackingdata"
 
@@ -8,19 +13,22 @@ bq_auth(email = "juanevaccaro@gmail.com")
 
 
 #Query a consultar
-sql <- "
+sql <- paste0("
 SELECT state
       ,state_fips_code
       ,SUM(confirmed_cases) confirmed_cases
       ,SUM(deaths) deaths
-FROM `bigquery-public-data.covid19_usafacts.summary`
-WHERE date = '2021-12-31'
+FROM `",bigquery_covid,"`
+WHERE date = '",year,"-12-31'
 GROUP BY state,state_fips_code
 "
+)
+
 
 #Extraccion de la data
 tb <- bq_project_query(billing_id, sql)
 covid_data <- bq_table_download(tb)
 
+if(nrow(covid_data) == 0) stop('No hay registros de Covid-19 para el year seleccionado (la informacion es consultada a cierre del year)')
 
 print('Paso 4: Completado exitosamente! . . . . . . . ')
